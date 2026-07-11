b10e17ea51036e446051ad5a8809b303c4b3dd34
Gibiamie <100677393+Gibiamie@users.noreply.github.com>
2026-07-11 15:27:03 +0400

    Update FirstStep service worker for Beta 0.3.3

--- firststep/service-worker.js
+++ firststep/service-worker.js
@@ -1,7 +1,6 @@
-const CACHE = 'firststep-beta-0.3.2-github';
+const CACHE = 'firststep-beta-0.3.3';
 const ASSETS = [
-  './', './index.html', './styles.css?v=0.3.2', './bundle-loader.js?v=0.3.2', './bundle.part1.txt', './bundle.part2.txt', './bundle.part3.txt', './bundle.part4.txt', './bundle.part5.txt', './bundle.part6.txt', './bundle.part7.txt', './bundle.part8.txt',
-  './manifest.webmanifest', './icon.svg'
+  './', './index.html', './styles.css?v=0.3.3', './app-0.3.3.js?v=0.3.3', './manifest.webmanifest?v=0.3.3', './icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
@@ -9,17 +8,38 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
   if (url.origin !== location.origin || event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate' || url.pathname.endsWith('/firststep/') || url.pathname.endsWith('/firststep/index.html')) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-      const copy = response.clone();
-      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(event.request)
+      .then((cached) => cached || fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html')))
   );
 });
