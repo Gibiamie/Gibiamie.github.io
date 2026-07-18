4dd87c9fbca0bb6659362e19bc160a72413c65c6
Gibiamie <100677393+Gibiamie@users.noreply.github.com>
2026-07-18 11:17:30 +0400

    chore: bump MIC mobile cache to v8

--- mic/sw.js
+++ mic/sw.js
@@ -1,5 +1,5 @@
-const CACHE='mic-mobile-v7';
-const CORE=['./','index.html','manifest.webmanifest','style.css?v=7','indicators.css?v=7','app-core.js?v=7','app-main.js?v=7','virtual.js?v=7','indicators.js?v=7'];
+const CACHE='mic-mobile-v8';
+const CORE=['./','index.html','manifest.webmanifest','style.css?v=8','indicators.css?v=8','app-core.js?v=8','app-main.js?v=8','virtual.js?v=8','indicators.js?v=8'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{
